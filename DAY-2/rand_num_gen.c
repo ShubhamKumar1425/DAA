18a1c54411578335b560d59aa699c461e3561541
Shubham Kumar <rkshubham108@gmail.com>
2025-12-10 08:39:44 +0530

    Add files via upload

---
+++ DAY-2/rand_num_gen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    srand(time(0));
+    for(int i=0; i<10;i++){
+        int random_num = (rand() % 100) +1;
+        printf("%d\n", random_num);
+    }
+    return 0;
+}
+
