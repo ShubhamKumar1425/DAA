b991dfd03457f326db7a83f01a992135e39ca961
Shubham Kumar <rkshubham108@gmail.com>
2025-12-12 16:09:10 +0530

    Add files via upload

---
+++ DAY-2/range.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int LOWER = 20;
+    int UPPER = 80;
+
+    srand(time(0));
+
+    for (int i = 0; i < 10; i++) {
+        int random_num = (rand() % (UPPER - LOWER + 1)) + LOWER;
+        printf("%d\n", random_num);
+    }
+
+    return 0;
+}
