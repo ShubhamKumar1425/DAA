255147ea4d394095d783e2afdb0f0ef9e180c348
Shubham Kumar <rkshubham108@gmail.com>
2025-12-15 20:48:28 +0530

    Add files via upload

---
+++ DAY-2/gen_in_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 500
+#define LOWER 20
+#define UPPER 80
+
+int main() {
+    int numbers[SIZE];
+    
+    srand(time(0));
+
+    for (int i = 0; i < SIZE; i++) {
+        numbers[i] = (rand() % (UPPER - LOWER + 1)) + LOWER;
+    }
+    printf("Generated Random Numbers:\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", numbers[i]);
+    }
+
+    return 0;
+}
